0fdff506bab8d4a7f0acad4e2869cf4d5e8bea53
Nikolay N Shmelev <aerograf@shmel.org>
2019-03-07 14:25:03 +0300

    Edit md5

--- admin/md5parser.php
+++ admin/md5parser.php
@@ -1,21 +1,24 @@
 <?php
-$dir = "";
-$file="indexscan.php";
-$verifyMessage="";
-			$checksum ="9f830dc5ac147aba24306869de07e57a"; // php checksum Linux
-			$checksumNT = "538d3f8d1a9279360a92ac1e4b8e410a"; // php checksum Winnt
-			$indexscanfile =  $dir . $file;
-            echo $checksum; 
-				if (md5_file($indexscanfile) != $checksum && md5_file($indexscanfile) != $checksumNT) {
-					$verifyMessage .= '<h3 style="color:red">'
+define('_CHECK_SUM', '9f830dc5ac147aba24306869de07e57a');   // php checksum Linux
+define('_CHECK_SUM_NT', '538d3f8d1a9279360a92ac1e4b8e410a');// php checksum Winnt
+    $dir  = '';
+    $file = 'indexscan.php';
+    $verifyMessage = '';
+	$checksum = _CHECK_SUM; 
+	$checksumNT = _CHECK_SUM_NT; 
+	$indexscanfile =  $dir . $file;
+
+		if (md5_file($indexscanfile) != $checksum && md5_file($indexscanfile) != $checksumNT) {
+		  $verifyMessage .= '<h3 style="color:red">'
           . $file
           . _AM_INDEXSCAN_NOTVERIFY
-          . "<img src='images/alert.png'></img>"
-          . "<br><br><a href='help.php#indexscan' title='"
+          . '<img src="images/alert.png"></img>'
+          . '<br><br><a href="help.php#indexscan" title="'
           . _FM_AM_HELP
-          . "'><img src='images/actions/help.png' alt='"
+          . '"><img src="images/actions/help.png" alt="'
           . _FM_AM_HELP
-          . "' style='border:none;'></a>&nbsp;&nbsp;";
-				}
-		elseif ( md5_file($indexscanfile) == $checksum ) {
-}
+          . '" style="border:none;"></a>&nbsp;&nbsp;';
+	}
+
+	elseif ( md5_file($indexscanfile) == $checksum ) {
+    }
